324aa5f336862cd588f085201c75950917648a00
Tony Kamillo <tonykamillo@gmail.com>
2020-03-30 19:47:17 -0300

    Updating setup version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='Flask-SQLAlchemy-Helpers',
-    version='1.0',
+    version='1.',
     url='https://github.com/tonykamillo/Flask-SQLAchemy-Helpers',
     license='BSD',
     author='Tony Kamillo',
